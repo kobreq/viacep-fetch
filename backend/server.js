38b52321f8bb4a0ccd26408f5d2f573aeef9b3c2
kobreq <gasteinbachs@gmail.com>
2026-08-12 11:17:39 -0300

    fix: api connection

--- backend/server.js
+++ backend/server.js
@@ -9,15 +9,17 @@ app.get('/api/mensagem', (req, res) => {
     res.json({ texto: "Olá do Servidor!"})
 })
 
-app.get('/cep/:cep', (req, res) => {
+app.get('/cep/:cep', async (req, res) => {
     const { cep } = req.params
 
     try {
         const resposta = await fetch(`https://viacep.com.br/ws/${cep}/json`)
         const dados = await resposta.json()
 
-        req.status(200).json(dados)
-    }
+        if(dados.erro) return res.status(404).json({ erro: "CEP não encontrado"})
+
+        res.status(200).json(dados)
+    } 
 
     catch(err) {
         res.status(500).json({ erro: "Erro de comunicação com VIACEP"})
