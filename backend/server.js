1de1edd6b20bad122e287c870c0129eb03e7fdda
kobreq <gasteinbachs@gmail.com>
2026-08-12 10:47:08 -0300

    feat: implementing VIACEP API use

--- backend/server.js
+++ backend/server.js
@@ -9,4 +9,19 @@ app.get('/api/mensagem', (req, res) => {
     res.json({ texto: "Olá do Servidor!"})
 })
 
-app.listen(3000)
+app.get('/cep/:cep', (req, res) => {
+    const { cep } = req.params
+
+    try {
+        const resposta = await fetch(`https://viacep.com.br/ws/${cep}/json`)
+        const dados = await resposta.json()
+
+        req.status(200).json(dados)
+    }
+
+    catch(err) {
+        res.status(500).json({ erro: "Erro de comunicação com VIACEP"})
+    }
+})
+
+app.listen(3001)
